c4eebca6f8cc1b4dd8c11a4ed6ff40f5c00e47ac
Abhishek D Jaiswar <devabhishekjaiswar@gmail.com>
2025-05-23 13:11:56 +0530

    Make the project tree structure according to the roadmap

---
+++ Step_01_Learn_the_basic/Lec_01_Things_to_know_in_Java/Topic_1_UserInputOutput.java
@@ -0,0 +1,7 @@
+package Step_01_Learn_the_basic.Lec_01_Things_to_know_in_Java;
+
+public class Topic_1_UserInputOutput {
+    public static void main(String[] args) {
+        System.out.println("User Input and Output");
+    }
+}
---
+++ Step_01_Learn_the_basic/Lec_02_Build_up_Logical_Thinking/Patterns.java
@@ -0,0 +1,7 @@
+package Step_01_Learn_the_basic.Lec_02_Build_up_Logical_Thinking;
+
+public class Patterns {
+    public static void main(String[] args) {
+        System.out.println("Pattern");
+    }
+}
